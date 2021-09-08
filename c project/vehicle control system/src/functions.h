39c4338bb9cb41aa5359d9ac9ee317828ef22eb1
Nader Atef <naderatef13@gmail.com>
2021-09-08 16:11:20 +0200

    Dynamic control system
    Final version

---
+++ c project/vehicle control system/src/functions.h
@@ -0,0 +1,7 @@
+void print_sensors_set_menu(void);
+void quit_system(void);
+void print_main(void);
+void system_response(char);
+void set_traffic();
+void set_roomtemp();
+void display_vehicle_state();
